d0d4a711ba90ca1af80f2f7dc2f26e516b3f919b
Wiktor Kycia <wiktor.kycia@gmail.com>
2023-05-21 15:30:16 +0200

    add form to sign in user contacts

---
+++ zapis_do_kontaktow/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+    <html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <title>Piekarnia</title>
+    </head>
+    <body>
+        <h1>Zapisz się</h1>
+        <form action="action.php" method="post">
+            <label for="imie">Imię</label>
+            <input name="imie" type="text">
+            <br>
+            <label for="nazwisko">Nazwisko</label>
+            <input name="nazwisko" type="text">
+            <br>
+            <label for="nrtel">Numer telefonu</label>
+            <input name="nrtel" type="tel">
+            <br>
+            <input type="submit" value="wyślij">
+        </form>
+        <a href="../index.html">Powrót do strony głównej</a>
+    </body>
+</html>
